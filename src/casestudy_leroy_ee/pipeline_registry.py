464a8b0e733a1bbef378c28987fd8cef22f0f3f1
Leroy Ee Bing Yao <leroyeby@gmail.com>
2025-12-02 22:22:22 +0800

    feat: added invoke llm pipeline to registry

--- src/casestudy_leroy_ee/pipeline_registry.py
+++ src/casestudy_leroy_ee/pipeline_registry.py
@@ -7,6 +7,9 @@ from .pipelines.pipelines_preprocessing import (
     create_preprocessing_full_pipeline,
     create_preprocessing_sqldb_pipeline,
 )
+from .pipelines.pipelines_invoke_llm import (
+    create_invoke_llm_full_pipeline,
+)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -36,11 +39,19 @@ def register_pipelines() -> dict[str, Pipeline]:
     pipelines["data_preprocessing_sqldb"] = create_preprocessing_sqldb_pipeline()
 
     # ----------------------------------
+    # Invoke LLM Pipelines
+    # ----------------------------------
+    # ----------------------------------
+    ### Invoke LLM Main Pipeline
+    # ----------------------------------
+    pipelines["invoke_llm_full"] = create_invoke_llm_full_pipeline()
+
+    # ----------------------------------
     # Full Main Pipeline
     # ----------------------------------
     pipelines["__default__"] = (
         create_preprocessing_full_pipeline()
-        # + create_invoke_llm_full_pipeline()
+        + create_invoke_llm_full_pipeline()
         # + create_generate_report_full_pipeline()
     )
 
